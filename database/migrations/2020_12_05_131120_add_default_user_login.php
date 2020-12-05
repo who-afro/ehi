f9e358c52996edaae58e9f6f94e5acc5ac7ea886
Stephen S. Musoke <ssmusoke@gmail.com>
2020-12-05 13:20:37 +0300

    Initial commit

---
+++ database/migrations/2020_12_05_131120_add_default_user_login.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDefaultUserLogin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        DB::table('users')->updateOrInsert([
+            'id' => '1',
+            'name' => 'System Admin',
+            'email' => 'ehi@styxtechgroup.com',
+            'password' => Hash::make('Admin123'),
+            'current_team_id' => "1"
+        ]);
+        DB::table('teams')->updateOrInsert([
+            'id' => '1',
+            'user_id' => '1',
+            'name' => 'System Team',
+            'personal_team' => '1'
+        ]);
+        Schema::enableForeignKeyConstraints();
+    }
+}
