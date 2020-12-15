6c5d276583457176ffb83090df0f9e173baac290
Stephen S. Musoke <ssmusoke@gmail.com>
2020-12-15 10:03:01 +0300

    Allow display of the header in cases when it is null

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,13 +22,12 @@
 <div class="min-h-screen bg-gray-100">
     @auth
         @livewire('navigation-dropdown')
-    @elseauth
     @endauth
 
     <!-- Page Heading -->
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {{ $header }}
+            {{ $header ?? '' }}
         </div>
     </header>
 
