a599b7647dd234c4529806e4174249a798f6608a
Stephen S. Musoke <ssmusoke@gmail.com>
2021-03-20 10:46:55 +0300

    Rename interventionLevel relation to levelofcare

--- app/Models/Intervention.php
+++ app/Models/Intervention.php
@@ -47,7 +47,7 @@ class Intervention extends Model
         return $this->belongsTo(AgeCohort::class);
     }
 
-    public function interventionLevel()
+    public function levelofcare()
     {
         return $this->belongsTo(LevelOfCare::class);
     }
