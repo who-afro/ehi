2fc1903cae2bb52cd18d7b00670a6e404b99b269
Stephen S. Musoke <ssmusoke@gmail.com>
2021-03-30 06:02:19 +0300

    Customize Nova to have the WHO look and feel

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -17,6 +17,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        Nova::style('ehi', asset('css/ehi.css'));
     }
 
     /**
