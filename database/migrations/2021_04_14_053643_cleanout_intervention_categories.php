4e3b88fdf861f80275c465787b36baac47d0fcbb
Stephen S. Musoke <ssmusoke@gmail.com>
2021-04-14 09:58:10 +0300

    Delete Existing Intervention Categories

---
+++ database/migrations/2021_04_14_053643_cleanout_intervention_categories.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CleanoutInterventionCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // delete the existing intervention categories
+        Schema::disableForeignKeyConstraints();
+        DB::table('intervention_intervention_category')->delete();
+        DB::table('intervention_categories')->delete();
+    }
+}
