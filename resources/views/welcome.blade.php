04cf9465993d2d4b9c1d42f3f658b5399168d73b
Stephen S. Musoke <ssmusoke@gmail.com>
2021-04-21 23:46:54 +0300

    Add the prose style for the content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-intervention-stats></x-intervention-stats>
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 prose">
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
                         Thematic areas
                     </h3>
