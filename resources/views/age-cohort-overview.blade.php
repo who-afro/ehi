08c3c52dc22f706d0f4eb39c698f2cb3c2abfee7
Stephen S. Musoke <ssmusoke@gmail.com>
2021-07-07 13:12:50 +0300

    Add descriptions of the different age groups

--- resources/views/age-cohort-overview.blade.php
+++ resources/views/age-cohort-overview.blade.php
@@ -11,12 +11,40 @@
         <a href="/age-cohort/1" title="Reproductive and newborn" class="flex items-center">
             <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
             Click here to view the Reproductive and newborn interventions </a>
-        <h2>Less than 5 years</h2>
+        <h2>Pediatric</h2>
         <p>
-            These are children below the age of 1 year
+            These are children between the ages of 1 to 5 years
         </p>
-        <a href="/age-cohort/2" title="< 5 years" class="flex items-center">
+        <a href="/age-cohort/2" title="Pediatric" class="flex items-center">
             <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
-            Click here to view the less than 5 years interventions </a>
+            Click here to view the Pediatric interventions </a>
+        <h2>Children</h2>
+        <p>
+            These are children who are between 5 to 11 years old
+        </p>
+        <a href="/age-cohort/3" title="< 5 years" class="flex items-center">
+            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
+            Click here to view interventions for children</a>
+        <h2>Teenagers and young adults</h2>
+        <p>
+            These are between 12 to 24 years
+        </p>
+        <a href="/age-cohort/4" title="Teenagers and young adults" class="flex items-center">
+            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
+            Click here to view interventions for Teenagers and young adults </a>
+        <h2>Adults</h2>
+        <p>
+            These are between 25 to 59 years
+        </p>
+        <a href="/age-cohort/5" title=Adults" class="flex items-center">
+            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
+            Click here to view interventions for adults </a>
+        <h2>Seniors</h2>
+        <p>
+            These are above 60 years of age
+        </p>
+        <a href="/age-cohort/6" title=Seniors" class="flex items-center">
+            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
+            Click here to view interventions for seniors </a>
     </div>
 </x-app-layout>
