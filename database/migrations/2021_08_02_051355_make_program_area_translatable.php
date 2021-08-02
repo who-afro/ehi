3d7a1ad9ca55360c0436722b9d9b64468450154e
Stephen S. Musoke <ssmusoke@gmail.com>
2021-08-02 12:53:17 +0300

    Make the program area translatable so that a description can be added

---
+++ database/migrations/2021_08_02_051355_make_program_area_translatable.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeProgramAreaTranslatable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::beginTransaction();
+        Schema::table('program_areas', function(Blueprint $table){
+            $table->text('name')->change();
+            $table->text('description')->change();
+        });
+
+        // move the name and text to JSON format
+        DB::statement("UPDATE program_areas set `name` = JSON_OBJECT('en', name), `description` = JSON_OBJECT('en', description)");
+
+        DB::commit();
+    }
+}
