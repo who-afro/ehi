9244bf5261edd56cf24482a6412f09041292058a
Stephen S. Musoke <ssmusoke@gmail.com>
2021-08-02 13:37:16 +0300

    Add ability to show descriptions including icons for the different program areas

--- resources/views/program-area-overview.blade.php
+++ resources/views/program-area-overview.blade.php
@@ -4,12 +4,16 @@
     </x-slot>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 prose">
-        The program areas in the digital toolkit are derived from the WHO Global burden of diseases
+        The program areas in the digital toolkit are derived from the WHO Global burden of diseases, click on a program area to show details
         <!-- accordian inspiration and code adapted from https://codepen.io/QJan84/pen/zYvRMMw -->
-        <div class="grid grid-cols-2 gap-2 sm:grid-cols-2 mt-4" x-data="{selected:null}">
+        <div class="mt-4" x-data="{selected:null}">
                 @foreach(App\Models\ProgramArea::has('conditions')->get() as $programArea)
-                    <div class="grid grid-cols-1 relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500 cursor-pointer" @click="selected !== {{ $programArea->id }} ? selected = {{ $programArea->id }} : selected = null">
-                        <div>{{ $programArea->name }}</div>
+                    <div class="grid grid-cols-2 relative rounded-lg border border-gray-300 bg-white px-6 py-5 my-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500 cursor-pointer" @click="selected !== {{ $programArea->id }} ? selected = {{ $programArea->id }} : selected = null; ">
+                        <div class="align-top">{{ $programArea->name }}
+                            <span x-bind:style="selected == {{ $programArea->id }} ? '' : 'display:none'" style="display: none;">
+                                {!! $programArea->description !!}
+                            </span>
+                        </div>
 
                         <div class="relative overflow-hidden transition-all max-h-0 duration-700" style=""
                              x-ref="container{{ $programArea->id }}"
