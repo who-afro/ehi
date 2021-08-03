f1e73a944fb117922117f3fc2a785950a79757a0
Stephen S. Musoke <ssmusoke@gmail.com>
2021-08-03 21:47:13 +0300

    Age chorts are loaded from the database

--- resources/views/age-cohort-overview.blade.php
+++ resources/views/age-cohort-overview.blade.php
@@ -2,49 +2,17 @@
     <x-slot name="header">
         Age Cohorts
     </x-slot>
-    <div class="max-w-7xl mx-auto ml-6 prose">
+    <div class="max-w-7xl mx-auto prose">
         <p>The age cohorts covered in this menu of interventions are</p>
-        <h2>Reproductive and newborn</h2>
-        <p>
-            These are children below the age of 1 year
-        </p>
-        <a href="/age-cohort/1" title="Reproductive and newborn" class="flex items-center">
-            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
-            Click here to view the Reproductive and newborn interventions </a>
-        <h2>Pediatric</h2>
-        <p>
-            These are children between the ages of 1 to 5 years
-        </p>
-        <a href="/age-cohort/2" title="Pediatric" class="flex items-center">
-            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
-            Click here to view the Pediatric interventions </a>
-        <h2>Children</h2>
-        <p>
-            These are children who are between 5 to 11 years old
-        </p>
-        <a href="/age-cohort/3" title="< 5 years" class="flex items-center">
-            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
-            Click here to view interventions for children</a>
-        <h2>Teenagers and young adults</h2>
-        <p>
-            These are between 12 to 24 years
-        </p>
-        <a href="/age-cohort/4" title="Teenagers and young adults" class="flex items-center">
-            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
-            Click here to view interventions for Teenagers and young adults </a>
-        <h2>Adults</h2>
-        <p>
-            These are between 25 to 59 years
-        </p>
-        <a href="/age-cohort/5" title=Adults" class="flex items-center">
-            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
-            Click here to view interventions for adults </a>
-        <h2>Seniors</h2>
-        <p>
-            These are above 60 years of age
-        </p>
-        <a href="/age-cohort/6" title=Seniors" class="flex items-center">
-            <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
-            Click here to view interventions for seniors </a>
+        @foreach(App\Models\AgeCohort::all() as $ageCohort)
+            <h2>{{ $ageCohort->name }}</h2>
+            <p>
+                {{ $ageCohort->description }}
+            </p>
+            <a href="/age-cohort/${{ $ageCohort->id }}" title="{{ $ageCohort->name }}" class="flex items-center">
+                <x-far-hand-point-right class="mr-3 h-6 w-6 text-blue-500 group-hover:text-blue-600"/>
+                Click here to view the {{ $ageCohort->name }} interventions
+            </a>
+        @endforeach
     </div>
 </x-app-layout>
