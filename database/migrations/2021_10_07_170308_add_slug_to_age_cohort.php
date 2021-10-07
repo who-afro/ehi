90c19c4a684332d43d5200bcab9846cc65158d4f
Stephen S. Musoke <ssmusoke@gmail.com>
2021-10-07 20:07:23 +0300

    Add slug to the age-cohorts

---
+++ database/migrations/2021_10_07_170308_add_slug_to_age_cohort.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSlugToAgeCohort extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('age_cohorts', function (Blueprint $table) {
+            $table->string('slug');
+        });
+    }
+}
