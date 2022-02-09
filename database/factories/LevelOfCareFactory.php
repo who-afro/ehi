cd78610d8fd0f5a9c1e391ea765f5b897f55562f
Shift <shift@laravelshift.com>
2022-02-09 10:52:41 +0000

    Remove unnecessary `$model` property

--- database/factories/LevelOfCareFactory.php
+++ database/factories/LevelOfCareFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class LevelOfCareFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = LevelOfCare::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
--- database/factories/PublicHealthFunctionFactory.php
+++ database/factories/PublicHealthFunctionFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class PublicHealthFunctionFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = PublicHealthFunction::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
