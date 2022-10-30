47058c9c3d2b4dddf499917e0817a94c1459cbd6
Stephen S. Musoke <ssmusoke@gmail.com>
2022-10-30 17:50:39 +0300

    Add an All option, which is the default for confirmed with evidence

--- resources/views/components/filters/confirmed-with-evidence.blade.php
+++ resources/views/components/filters/confirmed-with-evidence.blade.php
@@ -4,7 +4,14 @@
     </dt>
     <dd class="text-gray-700 px-4">
             <div class="flex items-start">
-                <div class="flex items-center flex-col">
+                <div class="flex items-left flex-col">
+                    <label>
+                        <!-- Zero-width space character, used to align checkbox properly -->
+                        &#8203;
+                        <input id="confirmed_with_evidence_all" type="radio" name="confirmed_with_evidence"
+                               class="form-radio indigo-600 border-2 rounded-md shadow-sm"
+                               wire:model="filters.confirmed_with_evidence" value="" checked="checked"/>&nbsp;&nbsp;All
+                    </label>
                     <label>
                     <!-- Zero-width space character, used to align checkbox properly -->
                     &#8203;
